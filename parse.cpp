e6ae3338094cbf3c26f250686987ae695d8455dc
babak <babak@localhost.localdomain>
2022-11-09 21:37:14 +0330

    some small cganges

--- parse.cpp
+++ parse.cpp
@@ -1,14 +1,13 @@
 #include "parse.h"
 
-Node* Parse::creatNode(int number, bool permission)
+Node* Parse::creatNode(int id, bool permission)
 {
     node_obj = new Node;
-    node_obj->setNumber(number);
+    node_obj->setId(id);
     //check if we're not created the root node yet
     if (this->allnodeslist.empty()){ // root node
         node_obj->setPermision(true);// root node is always accessable
-        if (!permission) std::cout << " [!] Root node must be always true\n";
-        node_obj->setParent(NULL);
+        if (!permission) std::cout << " [!] Root permission must be always true\n";
     else{ // we'e already in sub tree nodes
         
 
