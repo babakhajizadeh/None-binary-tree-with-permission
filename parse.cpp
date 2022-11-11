63e26012dae3a54d3ba6b38b3c854e67e5dae505
babak <babak@localhost.localdomain>
2022-11-11 03:46:57 +0330

    progress in class Parse methods implement.

--- parse.cpp
+++ parse.cpp
@@ -2,24 +2,35 @@
 
 bool Parse::parseNode(int id, bool permission) //Node already created only permission operation
 {
-    Node* m_currentNode;
     //check if we're not created the root node yet
-    if (allnodes.empty()) // root node
+    if (allnodes.empty()) // there is no root yet, need to be created
     { 
-        node_obj->setPermision(true);// root node is always accessable
-        if (!permission){ 
-            std::cout << " [!] Invalid imput; The root permission must be always true\n";
+        if (!permission or id != 1) // error. no root has defined
+        { 
+            std::cout << " [!] Invalid imput; The root permission or id is wrong\n";
             return false;
         }
+        if (permission and id == 1) // root node creation
+        {
+            node_obj = new Node;
+            node_obj->setId(1);
+            node_obj->setPermision(true);// root node is always accessable
+            allnodes.push_back(node_obj);
+        }
     }
-    else // sub-tree nodes
+    else // sub-tree nodes, only permision needs to be set (explicitly)
     { 
         Node* m_currentNode = NULL;
         for(auto node : allnodes) //finds nodes with id
         {
-            if (node->getId() == id) m_currentNode = node; 
+            if (node->getId() == id) m_currentNode = node;
+            break; 
         }
-        if (m_currentNode == NULL) std::cout << " [!] Node not found, Id not Exist";
+        if (m_currentNode == NULL) 
+        { 
+            std::cout << " [!] Node not found, Id not Exist";
+            return false;
+        }
         m_currentNode->setPermision(permission); //set permision to founded node
         return true;  
     }
@@ -27,13 +38,21 @@ bool Parse::parseNode(int id, bool permission) //Node already created only permi
 
 bool Parse::parseNode(int id, int parent_id) 
 {
+    // check if there is root node
+    if (allnodes.empty()){
+        std::cout << " [!] Invalid input; The root has not defined.";
+        return false;
+    }
     node_obj = new Node;
     node_obj->setId(id);
     Node* m_currentParent;
-    if (allnodes.empty()){
-        std::cout << " [!] Invalid input; The root has not defined.";
-        return flase;
-        
+    for (auto node : allnodes){
+        if (node->getId() == parent_id)
+        {
+            m_currentParent = node;
+            break;
+        }
     }
-
+    m_currentParent->setChild(node_obj);
+    node_obj->setPermission(); //parent's permission will be used
 }
